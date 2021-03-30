cac7184923954b7329c99cab00b012447c2e5fe3
Bean-Green <JBGrantII@gmail.com>
2021-03-29 22:38:18 -0400

    added temp and humidity

--- ArduinoMicrocontroller/ArduinoSprint3/ArduinoSprint3.ino
+++ ArduinoMicrocontroller/ArduinoSprint3/ArduinoSprint3.ino
@@ -1,33 +1,46 @@
+//initializations of the humidifier makr sure to add the adafruit library
+#include "DHT.h"
+#define DHTPIN 11
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+#define led 5
+
 int sensorValue;
 char command;
 String string;
-#define led 5
 int data = 0;
 bool fanFlip = true;
-bool sensFlip = false;
-bool autoFlip = false;
+bool autoFlip = true;
+bool atomizerFlip = true;
 
 void setup(){ 
+  
 Serial.begin(9600);       // sets the serial port to 9600
 pinMode(13, OUTPUT);     // sets the digital pin 13 as output
 pinMode(12, OUTPUT);
 pinMode(A0, INPUT);     // Grabs sensor readings
 
+//humidifier
+dht.begin();
+
 digitalWrite(13, HIGH); 
-digitalWrite(12, HIGH);
  }
  
 void loop()
 {
-  //if i remove this serial print my buttons do not work 
   //Serial.println(string); //debugger
-  if (sensFlip == false)
-  {
-     data = analogRead(A0);        // Assigns data readings to integer data
-     Serial.print("\n Air Quality in PPM is: ");
-     Serial.print(data);          // send this value to app
-  }
-  delay(500);                //Every 2 seconds it will send to java and new reading
+  data = analogRead(A0);        // Assigns data readings to integer data
+  float humi = dht.readHumidity(); // records the humidity 
+  delay(10);
+  float temp = dht.readTemperature(); //records the temperature
+  Serial.print("\n Air Quality in PPM is: ");
+  Serial.print(data);          // send this value to app
+  Serial.print("\n Air Humidity in %: ");
+  Serial.print(humi);
+  Serial.print("\n Air Temperature in Celcius  : ");
+  Serial.print(temp);
+  delay(1000);                //Every 2 seconds it will send to java and new reading
     
   if (Serial.available() > 0)
   {string = "";
@@ -63,21 +76,21 @@ void loop()
     fanFlip = true;
   } 
 
-//SENSOR CONTROLS
-   if(string =="SO" && sensFlip == true)
+//ATOMIZER CONTROLS
+   if(string =="HO" && atomizerFlip == true)
   {
-    SensOn();                   // see comment above  
+    atomOn();                   // see comment above  
     //Serial.println(string); //debug
     delay(100);
-    sensFlip = false;
+    atomizerFlip = false;
   }
 
-  if(string =="SF" && sensFlip == false)
+  if(string =="HF" && atomizerFlip == false)
   {
-    sensFlip = true;
-    SensOff();                   // see comment above  
+    atomOff();                   // see comment above  
     //Serial.println(string); //debug
     delay(100);
+    atomizerFlip = true;
   }
 
 //AUTOFAN CONTROLS
@@ -115,17 +128,17 @@ void fanOff()
 }
 
 
-void SensOn()
+void atomOn()
 {
-  digitalWrite(12,HIGH); // uses fan
-  Serial.println("\n\n  You have turned ON the Sensor");
+  digitalWrite(12,HIGH);
+  Serial.println("\n\n  You have turned ON the Humidifier");
   delay(10);
 }
 
-void SensOff()
+void atomOff()
 {
   digitalWrite(12,LOW);
-  Serial.println("\n\n  You have turned OFF the Sensor");
+  Serial.println("\n\n  You have turned OFF the Humidifier");
   delay(10);
 }
 
